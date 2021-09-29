8af050a69b9360fed9feab95d2f113606fb5f1b2
Ivan <ixtreme3@yandex.ru>
2021-09-29 19:19:50 +0700

    init front

---
+++ front/src/App.js
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from 'react';
+
+function Clock() {
+  const [date, setDate] = useState(new Date());
+
+  function refreshClock() {
+    setDate(new Date());
+  }
+
+  useEffect(() => {
+    const timerId = setInterval(refreshClock, 1000);
+    return function cleanup() {
+      clearInterval(timerId);
+    }
+  }, []);
+
+  return (
+    <span>
+      {date.toLocaleTimeString()}
+    </span>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <span>Hello world! It's </span>
+      <Clock />
+    </div>
+  );
+}
+
+export default App;
