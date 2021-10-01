0af4ef1cc6721f3e86cbf6ea97d15d89ab0333df
Anton <antoioly2000@gmail.com>
2021-10-01 17:28:03 +0700

    spring init

---
+++ back/src/main/java/ru/nsu/promocodesharebackend/PromocodeShareBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.promocodesharebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromocodeShareBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromocodeShareBackendApplication.class, args);
+    }
+
+}
---
+++ back/src/test/java/ru/nsu/promocodesharebackend/PromocodeShareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.promocodesharebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromocodeShareBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
