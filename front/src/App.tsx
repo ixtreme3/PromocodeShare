754bf7d30e0b7c340b6b874075573b606780edb6
Ivan <ixtreme3@yandex.ru>
2021-10-10 22:40:09 +0700

    minor changes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,34 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 import ButtonAppBar from './components/buttonAppBar';
-import { StyledEngineProvider } from '@material-ui/styled-engine-sc';
-import { Button } from '@material-ui/core';
-
-const StyledButtonAppBar = styled(ButtonAppBar)`
-  // doesn't work. but should it?
-  color: black;
-  background-color: white;
-  height: 80px;
-`;
-
-const StyledButton = styled(Button)`
-  //color: black; // doesn't work at all
-
-  && {
-    // works fine
-    color: black;
-  }
-`;
 
 function App() {
   return (
-    <StyledEngineProvider injectFirst>
-      <div className="App">
-        <StyledButtonAppBar />
-      </div>
-
-      <StyledButton>Click on me</StyledButton>
-    </StyledEngineProvider>
+    <div className="App">
+      <ButtonAppBar />
+    </div>
   );
 }
 
