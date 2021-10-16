055581f477d8c0670a2a633b9311d034a7d8fba3
Sergey Filonov <s.filonov@g.nsu.ru>
2021-10-16 13:07:18 +0000

    add footer

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ButtonAppBar from './components/buttonAppBar';
+import Footer from './components/footer'
 
 function App() {
   return (
     <div className="App">
       <ButtonAppBar />
+      <Footer />
     </div>
   );
 }
