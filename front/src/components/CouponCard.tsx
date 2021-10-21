f8018586638b0ca418fb3ecb70e01f099db7f0df
Ivan <ixtreme3@yandex.ru>
2021-10-21 17:42:54 +0700

    added CouponListWidget to AnonymousDashboard

--- front/src/components/CouponCard.tsx
+++ front/src/components/CouponCard.tsx
@@ -3,15 +3,24 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import { Box } from '@mui/material';
 
 export const CouponCard: React.FC<any> = (props) => (
-  <Card sx={{ display: 'flex', margin: '10px', ':hover': { outline: '2px dashed #4287f5' } }}>
-    <CardMedia
-      component="img"
-      sx={{ width: '175px', objectFit: 'contain' }}
-      image={props.input.picture}
-      alt="Failed to load picture"
-    />
+  <Card sx={{ display: 'flex', margin: '10px', ':hover': { outline: '1px solid #4287f5' } }}>
+    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+      <CardMedia
+        component="img"
+        sx={{ width: '175px', objectFit: 'contain', flex: 1 }}
+        image={props.input.picture}
+        alt="Failed to load picture"
+      />
+      <Typography variant="body2" color="text.secondary" align={'center'}>
+        Продавец:
+      </Typography>
+      <Typography variant="body2" color="text.secondary" align={'center'}>
+        {props.input.seller}
+      </Typography>
+    </Box>
     <CardContent sx={{ minWidth: '175px' }}>
       <Typography gutterBottom variant="h5">
         {props.input.promoCodeName}
