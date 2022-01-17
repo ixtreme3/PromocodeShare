c0e3f9d003ce019a55e30f83a12a947330cb9b3a
VitchenkoVladislav <v.vitchenko@trueengineering.ru>
2022-01-17 14:10:38 +0700

    ref #7262 - resolve merge conflicts

--- front/src/components/atoms/footer.tsx
+++ front/src/components/atoms/footer.tsx
@@ -1,15 +1,15 @@
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+import { AppBar } from '@material-ui/core';
 
 export default function Footer() {
   return (
     <AppBar position="static" color="primary">
-      <Container maxWidth="md">
-        <Toolbar>
-          <Typography variant="body1" color="inherit">
-            здесь будет footer текст
-          </Typography>
-        </Toolbar>
-      </Container>
+      {/*<Container maxWidth="md">*/}
+      {/*  <Toolbar>*/}
+      {/*    <Typography variant="body1" color="inherit">*/}
+      {/*      здесь будет footer текст*/}
+      {/*    </Typography>*/}
+      {/*  </Toolbar>*/}
+      {/*</Container>*/}
     </AppBar>
   );
 }
--- front/src/components/organizms/CouponListWidget.tsx
+++ front/src/components/organizms/CouponListWidget.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { CouponCard } from '../molecules/CouponCard';
-import { api } from '../../api/api';
 import { v4 as uuidv4 } from 'uuid';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { BeatLoader } from 'react-spinners';
+import { AxiosRestApplicationClient } from '../../api/PromocodeShareBackend';
 
 const outerDivStyles = {
   display: 'flex',
@@ -13,17 +15,38 @@ const innerDivStyles = {
   flexBasis: '60%',
 };
 
+const api: AxiosRestApplicationClient = new AxiosRestApplicationClient('http://localhost:8080/');
+
 export const CouponListWidget: React.FC = () => {
   const [coupons, setCoupons] = useState([{}]);
+  const [page, setPage] = useState(1);
 
-  useEffect(() => api.fetchCoupons(setCoupons), []);
+  useEffect(() => {
+    api
+      .findAllPaged({ page: 1, rowsPerPage: 10 })
+      .then((response) => setCoupons((prevCoupons) => [...prevCoupons, ...response.data]));
+  }, [page]);
 
   return (
     <div style={outerDivStyles}>
       <div style={innerDivStyles}>
-        {coupons.map((coupon) => (
-          <CouponCard key={uuidv4()} input={coupon} />
-        ))}
+        <InfiniteScroll
+          dataLength={coupons.length}
+          next={() => setPage((prevPage) => prevPage + 1)}
+          hasMore={true}
+          loader={
+            <div style={{ textAlign: 'center', marginTop: '10px' }}>
+              <BeatLoader color={'#1976d2'} />
+            </div>
+          }
+          endMessage={
+            <p style={{ textAlign: 'center' }}>
+              <b>That's it for now!</b>
+            </p>
+          }
+        >
+          {coupons.map((coupon) => <CouponCard key={uuidv4()} input={coupon} />).slice(1)}
+        </InfiniteScroll>
       </div>
     </div>
   );
